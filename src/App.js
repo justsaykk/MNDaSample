1334280eb0a06e43ae5870726d541573c60d41de
justsaykk <kaikein.woo@gmail.com>
2022-08-19 12:12:28 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from "./Navigation/NavBar";
+import yes from "./Resources/sound/yes_male.mp3";
+import no from "./Resources/sound/no_male.mp3";
+import { Howl, Howler } from 'howler';
+
+const playSound = (clip) => {
+  const sound = new Howl({
+    src: [clip]
+  });
+
+  sound.play();
+  Howler.volume(1.0);
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="body-container">
+        <div className='navBar'>
+          <NavBar />
+        </div>
+        <div className='content-container'>
+          <div className='yesNo'>
+            <table className='yesNo-table'>
+              <thead>
+                <tr>
+                  <td>
+                    <button onClick={() => { playSound(yes) }}>Yes / 是</button>
+                  </td>
+                  <td>
+                    <button onClick={() => { playSound(no) }}>No / 不是</button>
+                  </td>
+                </tr>
+              </thead>
+            </table>
+          </div>
+          <div className='directionals'>
+            <table className='directionals-table'>
+              <tr>
+                <td></td>
+                <td>
+                  <button>Up / 上</button>
+                </td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>
+                  <button>Left / 左</button>
+                </td>
+                <td></td>
+                <td>
+                  <button>Right / 右</button>
+                </td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>
+                  <button>Down / 下</button>
+                </td>
+                <td></td>
+              </tr>
+            </table>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Navigation/NavBar.js
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return (
+        <div>
+            <ul>
+                <li><a href="default.asp">Home</a></li>
+                <li><a href="contact.asp">Contact</a></li>
+                <li><a href="about.asp">About</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar;
